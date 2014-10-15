dd5e1041356719744503997c244cddf4f54e71c7
Gudmundur Adalsteinsson <ofpgummi@yahoo.com>
2014-10-15 23:20:57 +0000

    remove v0.2 support and fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,27 @@
 using Wavelets
 using Base.Test
 
-# v0.2
-if !isdefined(:vecnorm)
-    function vecnorm(s)
-        norm(s[:])
+# modified from Base.Test
+function test_vecnorm_eq(va, vb, Eps, astr, bstr)
+    if length(va) != length(vb)
+        error("lengths of ", astr, " and ", bstr, " do not match: ",
+              "\n  ", astr, " (length $(length(va))) = ", va,
+              "\n  ", bstr, " (length $(length(vb))) = ", vb)
+    end
+    diff = vecnorm(va - vb)
+
+    if !isnan(Eps) && !(diff <= Eps)
+        sdiff = string("|", astr, " - ", bstr, "| <= ", Eps)
+        error("assertion failed: ", sdiff,
+              "\n  difference = ", diff, " > ", Eps)
     end
 end
+macro test_vecnorm_eq_eps(a, b, c)
+    :(test_vecnorm_eq($(esc(a)), $(esc(b)), $(esc(c)), $(string(a)), $(string(b))))
+end
+
+
 
 include("util.jl")
 include("transforms.jl")
-
 
--- test/transforms.jl
+++ test/transforms.jl
@@ -10,31 +10,30 @@ name = "filter"
 data = vec(readdlm(joinpath(dirname(@__FILE__), "data", "filter1d_data.txt"),'\t'))
 data2 = readdlm(joinpath(dirname(@__FILE__), "data", "filter2d_data.txt"),'\t')
 
+stderr = 1e-9*sqrt(length(data))
+stderr2 = 1e-9*sqrt(length(data2))
+
 for i = 1:length(wname)
     for num = 1:length(wnum[i])
-        
+        # expected transforms
         ye = vec(readdlm(joinpath(dirname(@__FILE__), "data", string(name,"1d_",wname[i],wnum[i][num],".txt")),'\t'))
         ye2 = readdlm(joinpath(dirname(@__FILE__), "data", string(name,"2d_",wname[i],wnum[i][num],".txt")),'\t')
         
         wn = wnum[i][num]
         wn!=0 && (wt = POfilter(wname[i],wvm[i][num]))
         wn==0 && (wt = POfilter(lowercase(wname[i][1:4])))
+        # transform data
         y = fwt(data, wt)
         y2 = fwt(data2, wt)
-        #println(wname[i],wn,' ',wvm[i][num], ' ',vecnorm(y - ye))
-        #println(vecnorm(iwt(y,wt) - data))
-        #println(wname[i],wn,' ',wvm[i][num], ' ',vecnorm(y2 - ye2))
-        #println(vecnorm(iwt(y2,wt) - data2))
-        #println(abs(vecnorm(data)-vecnorm(y)))
-        #println(abs(vecnorm(data2)-vecnorm(y2)))
-        @test vecnorm(y - ye) < 5e-9
-        @test vecnorm(y2 - ye2) < 5e-9
+        
+        @test_vecnorm_eq_eps y ye stderr
+        @test_vecnorm_eq_eps y2 ye2 stderr2
         # a few bad cases have quite large norms
         if wname[i] != "Battle" && (wname[i] != "Coiflet" && wvm[i][num]==10)
             @test abs(vecnorm(data)-vecnorm(y)) < 1e-9
             @test abs(vecnorm(data2)-vecnorm(y2)) < 1e-9
-            @test vecnorm(iwt(y,wt) - data) < 5e-7
-            @test vecnorm(iwt(y,wt) - data) < 5e-7
+            @test_vecnorm_eq_eps iwt(y,wt) data stderr*100
+            @test_vecnorm_eq_eps iwt(y2,wt) data2 stderr2*100
         end
     end
 end
@@ -45,6 +44,8 @@ for WT in ("db1","db2")
     wls = GPLS(WT)
     n = 64
     x = randn(n)
+    stderr = 1e-10*sqrt(n)
+    stderr2 = 1e-10*sqrt(n*n)
     
     x2 = copy(x)
     tmp = zeros(n)
@@ -55,16 +56,16 @@ for WT in ("db1","db2")
         yls = fwt(x, L, wls)
         dwt!(x2, L, wls, true, tmp, oopc=true, oopv=tmpsub)
         
-        @test_approx_eq yf yls
-        @test_approx_eq yf x2
+        @test_vecnorm_eq_eps yf yls stderr
+        @test_vecnorm_eq_eps yf x2 stderr
         
         ytf = iwt(yf, L, wf)
         ytls = iwt(yls, L, wls)
         dwt!(x2, L, wls, false, tmp, oopc=true, oopv=tmpsub)
         
-        @test_approx_eq ytf x
-        @test_approx_eq ytls x
-        @test_approx_eq x2 x
+        @test_vecnorm_eq_eps ytf x stderr
+        @test_vecnorm_eq_eps ytls x stderr
+        @test_vecnorm_eq_eps x2 x stderr
         
     end
     
@@ -74,13 +75,14 @@ for WT in ("db1","db2")
         yf = fwt(x, L, wf)
         yls = fwt(x, L, wls)
         
+        @test_vecnorm_eq_eps yf yls stderr2
         @test_approx_eq yf yls
         
         ytf = iwt(yf, L, wf)
         ytls = iwt(yls, L, wls)
         
-        @test_approx_eq ytf x
-        @test_approx_eq ytls x
+        @test_vecnorm_eq_eps ytf x stderr2
+        @test_vecnorm_eq_eps ytls x stderr2
         
     end
 end
@@ -116,71 +118,51 @@ x = randn(16)
 
 # ============= types and sizes ================
 
-# 1-d
-n = 8
-wf = POfilter("db2")
-L = 2
-ft = Float64; x = ft[1:n]; y=fwt(x,wf);
-@test Array{ft,1} == typeof(y) && length(y) == n
-ft = Float32; x = ft[1:n]; y=fwt(x,wf);
-@test Array{ft,1} == typeof(y) && length(y) == n
-ft = Int64; x = ft[1:n]; y=fwt(x,wf);
-@test Array{typeof(float(x[1])),1} == typeof(y) && length(y) == n
-ft = Int32; x = ft[1:n]; y=fwt(x,wf);
-@test Array{typeof(float(x[1])),1} == typeof(y) && length(y) == n
+function makefwt(ft::Type, n, wf, L)
+	x0 = rand(-5:5, n)
+	x = zeros(ft, n)
+	copy!(x,x0)
+	return (x, fwt(x, wf, L))
+end
 
-ft = Int64
-x = ft[1:n]
-@test_approx_eq fwt(x,wf,2) fwt(x,2,wf)
+# 1-d
+n = 8; wf = POfilter("db2"); L = 2
+sett = (n,wf,L)
+
+ft = Float64; x, y = makefwt(ft, sett...)
+@test Array{ft,1} == typeof(y) && length(y) == n
+ft = Float32; x, y = makefwt(ft, sett...)
+@test Array{ft,1} == typeof(y) && length(y) == n
+ft = Int64; x, y = makefwt(ft, sett...)
+@test Array{typeof(float(x[1])),1} == typeof(y) && length(y) == n
+ft = Int32; x, y = makefwt(ft, sett...)
+@test Array{typeof(float(x[1])),1} == typeof(y) && length(y) == n
 @test_approx_eq fwt(x,wf) fwt(float(x),wf)
-ft = Float64
-x = ft[1:n]
 @test_approx_eq fwt(x,wf,2) fwt(x,2,wf)
 
 
 # 2-d
-n = 8
-wf = POfilter("db2")
-L = 2
-ft = Float64; x = ft[1:n]; y=fwt(x*x',wf);
+sett = ((n,n),wf,L)
+ft = Float64; x, y = makefwt(ft, sett...)
 @test Array{ft,2} == typeof(y) && length(y) == n*n
-ft = Float32; x = ft[1:n]; y=fwt(x*x',wf);
+ft = Float32; x, y = makefwt(ft, sett...)
 @test Array{ft,2} == typeof(y) && length(y) == n*n
-ft = Int64; x = ft[1:n]; y=fwt(int(x*x'),wf);
+ft = Int64; x, y = makefwt(ft, sett...)
 @test Array{typeof(float(x[1])),2} == typeof(y) && length(y) == n*n
-ft = Int32; x = ft[1:n]; y=fwt(int(x*x'),wf);
+ft = Int32; x, y = makefwt(ft, sett...)
 @test Array{typeof(float(x[1])),2} == typeof(y) && length(y) == n*n
-
-ft = Int64
-x = ft[1:n]
-@test_approx_eq fwt(x*x',wf,2) fwt(x*x',2,wf)
-@test_approx_eq fwt(x*x',wf) fwt(float(x*x'),wf)
-ft = Float64
-x = ft[1:n]
-@test_approx_eq fwt(x*x',wf,2) fwt(x*x',2,wf)
-
+@test_approx_eq fwt(x,wf) fwt(float(x),wf)
+@test_approx_eq fwt(x,wf,2) fwt(x,2,wf)
 
 
 # ============= error tests ================
 
-type wunknownt <: WaveletType
-    a::Int
-end
-uwt = wunknownt(1)
+type wunknownt <: WaveletType end
+uwt = wunknownt()
+EE = ErrorException
+@test_throws EE fwt(randn(4),uwt)
+@test_throws EE fwt(randn(4,4),uwt)
 
-if VERSION < v"0.3.0-"  # v0.2 support
-    tests=quote
-    @test_throws fwt(randn(4),uwt)
-    @test_throws fwt(randn(4,4),uwt)
-    end
-    eval(tests)
-else
-    EE = ErrorException
-    tests=quote
-    @test_throws EE fwt(randn(4),uwt)
-    @test_throws EE fwt(randn(4,4),uwt)
-    end
-    eval(tests)
-end
+
 
 
