30854da81966874f61e5bb1693b64a8acb8f5eff
Gudmundur Adalsteinsson <ofpgummi@yahoo.com>
2017-04-03 21:20:47 +0000

    Move submodules

--- src/Wavelets.jl
+++ src/Wavelets.jl
@@ -2,11 +2,11 @@ __precompile__()
 
 module Wavelets
 
-include("Util.jl")
-include("WT.jl")
-include("Transforms.jl")
-include("Threshold.jl")
-include("Plot.jl")
+include("mod/Util.jl")
+include("mod/WT.jl")
+include("mod/Transforms.jl")
+include("mod/Threshold.jl")
+include("mod/Plot.jl")
 
 using Reexport
 @reexport using .Util, .WT, .Transforms, .Threshold, .Plot
