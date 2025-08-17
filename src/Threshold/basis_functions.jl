84d6da092d7dfd96a3ab82eb35e1c5c843df8a28
wheeheee <104880306+wheeheee@users.noreply.github.com>
2025-08-17 19:11:15 +0800

    improve denoise, nspin2circ, threshold

--- src/Threshold/basis_functions.jl
+++ src/Threshold/basis_functions.jl
@@ -12,35 +12,40 @@ function matchingpursuit(x::AbstractVector, f::Function, ft::Function, tol::Real
     r = x
     n = 1
 
-    if !oop
-        y = zeros(eltype(x), length(ft(x)))
-    else # out of place functions f and ft
+    if oop  # out of place functions f and ft
         y = zeros(eltype(x), N)
         tmp = similar(x, N)
         ftr = similar(x, N)
         aphi = similar(x, length(x))
+    else
+        y = zeros(eltype(x), length(ft(x)))
     end
     spat = zeros(eltype(x), length(y))  # sparse for atom computation
     nmax == -1 && (nmax = length(y))
 
     while norm(r) > tol && n <= nmax
         # find largest inner product
-        !oop && (ftr = ft(r))
-        oop && ft(ftr, r, tmp)
-        i = findmaxabs(ftr)
+        if oop
+            ft(ftr, r, tmp)  # compute f^T(r) in place
+        else
+            ftr = ft(r)  # compute f^T(r)
+        end
+        ftri, i = findmax(abs, ftr)
 
         # project on i-th atom
-        spat[i] = ftr[i]
-        !oop && (aphi = f(spat))
-        oop && f(aphi, spat, tmp)
+        spat[i] = ftri
+        if oop
+            f(aphi, spat, tmp)  # compute aphi = f(spat) in place
+        else
+            aphi = f(spat)  # compute aphi = f(spat)
+        end
         spat[i] = 0
 
         # update residual, r = r - aphi
         broadcast!(-, r, r, aphi)
 
-        y[i] += ftr[i]
+        y[i] += ftri
         n += 1
     end
     return y
 end
-findmaxabs(x::AbstractVector) = findmax(abs, x)[2]
